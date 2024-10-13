14462efb017b9bc111de12863cc301abbc657f3b
Syahid Reza Mutahari <syahidreza@gmail.com>
2024-10-13 20:41:50 +0700

    feat: add 03-values

---
+++ 03-values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1 + 1 = ", 1 + 1)
+	fmt.Println("0.3334 + 0.6667 = ", 0.3334 + 0.6667)
+
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+}
